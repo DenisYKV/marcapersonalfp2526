4d160fdc692b6cb45859096e8dd48783ea3f544f
Denis <5945069@alu.murciaeduca.es>
2025-11-04 10:04:32 +0100

    Ejercicio rutas resuelto

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ('Pantalla principal');
 });
+
+Route::get('/login', function () {
+    return ('Login usuario');
+});
+
+Route::get('/logout', function () {
+    return ('Logout usuario');
+});
+
+Route::prefix('proyectos')->group(function () {
+    Route::get('/', function () {
+        return ('Listado proyectos');
+    });
+    Route::get('/show/{id}', function ($id) {
+         return view("Vista detalle proyecto $id");
+    });
+    Route::get('/create', function () {
+         return ('Añadir proyecto');
+    });
+    Route::get('/edit/{id}', function ($id) {
+         return ("Modificar proyecto $id");
+    });
+})-> where('id','[0-9]+');
+
+Route::get('/perfil/{id?}', function ($id = 'propio') {
+    return ("Visualizar el currículo de $id");
+})-> where('id','[0-9]+');
