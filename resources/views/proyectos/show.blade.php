8471aa62f1b1406c09f45db5cf70f8075e58ba10
Denis <5945069@alu.murciaeduca.es>
2025-11-19 09:46:50 +0100

    primera actualizacion botones en el show

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -2,7 +2,47 @@
 
 @section('content')
 
-    Vista detalle proyecto {{$id}}
+    <div class="row m-4">
 
+        <div class="col-sm-4">
+
+
+            <img src="/images/mp-logo.png" style="height:200px" />
+
+        </div>
+        <div class="col-sm-8">
+
+            <h3><strong>Nombre: </strong>{{ $proyecto['nombre'] }}</h3>
+            <h4><strong>Dominio: </strong>
+                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}">
+                    http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}
+                </a>
+            </h4>
+            <h4>Metadatos
+                @foreach ($proyecto['metadatos'] as $i => $dato)
+                    <li>{{ $i }}: {{ $dato }}</li>
+                @endforeach
+            </h4>
+            <p>
+                @if ($proyecto['metadatos']['calificacion'] >= 5)
+                    aprobado
+                    <br>
+                    <button style="background-color: red" type="button" id="suspender">Suspender proyecto</button>
+                @endif
+                @if ($proyecto['metadatos']['calificacion'] < 5)
+                    suspenso
+                    <br>
+                    <button style="background-color: blue" type="button" id="aprobar">Aprobar proyecto</button>
+                @endif
+            </p>
+            <ul class="actions">
+                <li><a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getEdit'],['id' => $id])}}"
+                        class="button alt">Editar proyecto</a></li>
+
+                <li><a href="{{ action([App\Http\Controllers\ProyectosController::class, 'getIndex']) }}"
+                        class="button alt">volver al inicio</a></li>
+            </ul>
+
+        </div>
+    </div>
 @stop
-
