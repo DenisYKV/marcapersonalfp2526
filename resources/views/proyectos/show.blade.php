3bec1d5df39ccbb097e739840894091a2c90cb51
Denis <5945069@alu.murciaeduca.es>
2025-12-10 09:26:14 +0100

    primera autentificacion

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -7,7 +7,7 @@
 
         <div class="col-sm-4">
 
-            <img src="/images/mp-logo.png" style="height:200px"/>
+            <img src="/images/mp-logo.png" style="height:200px" />
 
         </div>
         <div class="col-sm-8">
@@ -20,30 +20,35 @@
             </h4>
             <h4><strong>Docente: </strong>{{ $proyecto->docente_id }}</h4>
             <p><strong>Metadatos: </strong>
-                <ul>
-                    @foreach (unserialize($proyecto->metadatos) as $indice => $metadato)
-                        <li>{{ $indice }}: {{ $metadato }}</li>
-                    @endforeach
-                </ul>
+            <ul>
+                @foreach (unserialize($proyecto->metadatos) as $indice => $metadato)
+                    <li>{{ $indice }}: {{ $metadato }}</li>
+                @endforeach
+            </ul>
             </p>
             <p><strong>Estado: </strong>
-                @if(unserialize($proyecto->metadatos)['calificacion'] >= 5)
+                @if (unserialize($proyecto->metadatos)['calificacion'] >= 5)
                     Proyecto aprobado
                 @else
                     Proyecto suspenso
                 @endif
             </p>
+            @auth
+            // Solo los usuarios autenticados pueden ver los botones de aprobar/suspender y editar
 
-            @if(unserialize($proyecto->metadatos)['calificacion'] >= 5)
-                <a class="btn btn-danger" href="#">Suspender proyecto</a>
-            @else
-                <a class="btn btn-primary" href="#">Aprobar proyecto</a>
-            @endif
-            <a class="btn btn-warning" href="{{ action([App\Http\Controllers\ProyectosController::class, 'getEdit'], ['id' => $id]) }}">
-                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                Editar proyecto
-            </a>
-            <a class="btn btn-outline-info" href="{{ action([App\Http\Controllers\ProyectosController::class, 'getIndex']) }}">
+                @if (unserialize($proyecto->metadatos)['calificacion'] >= 5)
+                    <a class="btn btn-danger" href="#">Suspender proyecto</a>
+                @else
+                    <a class="btn btn-primary" href="#">Aprobar proyecto</a>
+                @endif
+                <a class="btn btn-warning"
+                    href="{{ action([App\Http\Controllers\ProyectosController::class, 'getEdit'], ['id' => $id]) }}">
+                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                    Editar proyecto
+                </a>
+            @endauth
+            <a class="btn btn-outline-info"
+                href="{{ action([App\Http\Controllers\ProyectosController::class, 'getIndex']) }}">
                 Volver al listado
             </a>
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProyectosController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +17,43 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/', [HomeController::class, 'getHome'])
+    ->name('home');
+
+
+// ----------------------------------------
+Route::prefix('proyectos')->group(function () {
+    Route::get('/', [ProyectosController::class, 'getIndex']);
+
+    Route::get('create', [ProyectosController::class, 'getCreate']);
+
+
+    Route::get('show/{id}', [ProyectosController::class, 'getShow'])->where('id', '[0-9]+');
+
+    Route::get('edit/{id}', [ProyectosController::class, 'getEdit'])
+    ->where('id', '[0-9]+');
+
+
+    Route::post('store', [ProyectosController::class, 'store']);
+
+
+    Route::put('update/{id}', [ProyectosController::class, 'update'])
+    ->where('id', '[0-9]+');
+
+    Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('create', [ProyectosController::class, 'getCreate']);
+         Route::get('edit/{id}', [ProyectosController::class, 'getEdit'])
+        ->where('id', '[0-9]+');
+        Route::post('store', [ProyectosController::class, 'store']);
+         Route::put('update/{id}', [ProyectosController::class, 'update'])
+        ->where('id', '[0-9]+');
+        Route::delete('destroy/{id}', [ProyectosController::class, 'destroy'])
+        ->where('id', '[0-9]+');
+    });
+
 });
 
 require __DIR__.'/auth.php';
